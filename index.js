5a9c47d0d54ff9a6da6799cdc14c7b6d4fd0e3f2
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 04:19:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var array=[];
   for(let i=0;i===musicians.length;i++){
     let musician = musicians[i];
     let instrument = instruments[i];
-    array.push(musician+instrument);
+    array.push(musician+" plays "+instrument);
   }
   return array;
 }
