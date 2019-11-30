5fb5ca5eb5896f4b0887b7d0bef4d8c2e91f4111
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 04:01:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
 var array=[];
-  for(i=0;i===musicians.length;i++){
+  for(let i=0;i===musicians.length;i++){
     array.push(musicians[i]+instruments[i]);
   }
   return array;
